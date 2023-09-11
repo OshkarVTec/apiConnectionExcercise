6e1ebc3c18bdc9d8f03bd7ca5af5b5a1905d6ebc
OshkarVTec <a01275287@tec.mx>
2023-09-11 15:35:53 -0600

    change style

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,26 @@ import classes from "./App.module.css";
 
 function App() {
 	const [filter, setFilter] = useState("family");
-  const handleFilter = (event) => {
-    setFilter(event.target.value);
-  }
+	const handleFilter = (event) => {
+		setFilter(event.target.value);
+	};
 
 	return (
 		<main className={classes.container}>
 			<h1 className={classes.header}>Fruit data search</h1>
 			<p>Made by Oskar Adolfo Villa López. Data from FruityVice API.</p>
-      <section className={classes.formContainer}>
-        <label for="filter">Filter by: </label>
-        <select value={filter} onChange={handleFilter}>
-          <option value="family">Family</option>
-          <option value="genus">Genus</option>
-          <option value="name">Name</option>
-        </select>
-      </section>
+			<section className={classes.formContainer}>
+				<label for="filter">Filter by: </label>
+				<select
+					value={filter}
+					onChange={handleFilter}
+					className={classes.selector}
+				>
+					<option value="family">Family</option>
+					<option value="genus">Genus</option>
+					<option value="name">Name</option>
+				</select>
+			</section>
 		</main>
 	);
 }
